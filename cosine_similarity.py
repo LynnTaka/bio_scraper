46e01095ce8d0b415133d9d135487b462f411881
LynnTaka <tigerlynn11@gmail.com>
2024-05-06 16:42:01 -0700

    added comments for understanding

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -6,8 +6,11 @@ def calculate_similarity(query_vector, faculty_vectors):
     return similarity_scores
 
 def get_top_five_most_similar(sim_scores, faculty_pages):
+    # put together scores and the corresponding faculty pages
     scored_pages = list(zip(sim_scores[0], faculty_pages))
+    # sort in descending order
     ranked_pages = sorted(scored_pages, key=lambda x: x[0], reverse=True)
+    # get top 5 most similar
     top_five = ranked_pages[:5]
 
     return top_five
